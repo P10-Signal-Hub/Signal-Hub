248c6683fa83e150a94f043adf8cc2fcfea2ffe1
BenBabicka <ben.babicka@gmail.com>
2025-09-26 00:04:17 +1000

    Improved task creation
    - Improved task creation to create multiple tasks.
    - Reduced number of prompts for task creation
    - Improved the execution speed of the models
    - Changed device selection to use CUDA first if available then CPU
    - Change Event.py __init__ parameters to be assigned
    - Changed templated conversations to an array and have a conversation picked at random

--- AI-Agent/Event.py
+++ AI-Agent/Event.py
@@ -1,11 +1,11 @@
 from EventType import EventType
 
 class Event:
-    def __init__(self, event_type:EventType, title:str, description:str, metadata:dict):
+    def __init__(self, event_type:EventType, title:str = '', description:str = '', metadata:dict = {}):
         self.eventType = event_type
         self.title = title
         self.description = description
         self.metadata = metadata
 
     def __repr__(self):
-        return f"Event(\nEvent Type:{self.eventType}\nTitle: {self.title}\nDescription: {self.description}\nMetadata: {self.metadata}\n)"
+        return f"{'-' * 20}Event{'-' * 20}\n-Event Type:{self.eventType}\n-Title: {self.title}\n-Description: {self.description}\n-Metadata: {self.metadata}\n{'-' * 45}\n"
